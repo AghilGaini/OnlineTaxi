0fb0efa3ff204fbf9aed9a42b596547d1d7a671c
asma <asma.gaini@yahoo.com>
2022-05-05 03:39:08 +0430

    libman : jquery,twitterBootstrap,SweetAlert

---
+++ Database.Domain/ViewModels/RegisterAccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Domain.ViewModels
+{
+    public class RegisterAccountViewModel
+    {
+        [Required]
+        [Display(Name = "نام کاربری")]
+        public string Username { get; set; }
+    }
+}
---
+++ OnlineTaxi.Site/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineTaxi.Site.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterAccountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+        }
+
+    }
+}
