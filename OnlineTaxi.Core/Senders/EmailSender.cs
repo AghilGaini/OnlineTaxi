41c505ee1cacc4a9a7ae53f0dfc3be0e8e711149
a.gaeini <a.gaeini@AGHIL-PC>
2022-05-10 17:10:21 +0430

    start send mail class developing

--- OnlineTaxi.Core/Senders/EmailSender.cs
+++ OnlineTaxi.Core/Senders/EmailSender.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace OnlineTaxi.Core.Senders
 {
@@ -10,7 +12,24 @@ namespace OnlineTaxi.Core.Senders
     {
         public static bool Send(string to, string subject, string body)
         {
-            //TODO
+            MimeMessage message = new MimeMessage();
+
+            MailboxAddress fromMailAddress = new MailboxAddress("aghil", "aghil1373@gmail.com");
+            message.From.Add(fromMailAddress);
+
+            MailboxAddress toMailAddress = new MailboxAddress("User", to);
+            message.To.Add(toMailAddress);
+
+            message.Subject = subject;
+
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = body;
+
+            message.Body = bodyBuilder.ToMessageBody();
+
+
+            SmtpClient smtpClient = new SmtpClient();
+
             return true;
         }
     }
