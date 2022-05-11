dc3b63e18d5bda18aaf02c40c589ede7aa95af99
a.gaeini <a.gaeini@AGHIL-PC>
2022-05-11 16:23:29 +0430

    some change

---
+++ Database.Domain/Entities/EmailSettingDomain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Domain.Entities
+{
+    public class EmailSettingDomain
+    {
+        [Required]
+        public long Id { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string SmtpServer { get; set; }
+        [Required]
+        public int SmtpPort { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
